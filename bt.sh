451c68a3930af3027bc47a34fe03005787d37e70
xiao <K5IFzgsHyFkgN2rl7B04RB+xiao@noreply.cnb.cool>
2024-11-02 12:53:00 +0800

    feat: 启动时启动lnmp环境

--- bt.sh
+++ bt.sh
@@ -1,5 +1,33 @@
-/etc/init.d/bt restart
-pkill crond
-/sbin/crond
-/usr/sbin/sshd -D &
+#!/bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+init_path=/etc/init.d
+Root_Path=`cat /var/bt_setupPath.conf`
+Setup_Path=$Root_Path/server/mysql
+Data_Path=$Root_Path/server/data
+
+soft_start(){
+    ${init_path}/nginx start
+    ${init_path}/php-fpm-83 start
+    ${init_path}/bt restart
+    pkill crond
+    /sbin/crond
+    /usr/sbin/sshd -D &
+}
+
+is_empty_Data(){
+    return `ls -A ${Data_Path}/|wc -w`
+}
+
+start_mysql(){
+    chown -R mysql:mysql ${Data_Path}
+    chgrp -R mysql ${Setup_Path}/.
+    ${init_path}/mysqld start
+    rm -f /init_mysql.sh
+}
+
+soft_start > /dev/null
+is_empty_Data > /dev/null
+start_mysql > /dev/null
 tail -f /dev/null
